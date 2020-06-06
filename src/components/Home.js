4305d6b6556259850648391d6096a6b9dd83af9d
eminaydin <eminaydin93@yahoo.com>
2020-06-07 01:04:14 +0200

    Home page text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import Typist from 'react-typist';
-import "react-typist/dist/Typist.css"
-
-
+import React from "react";
+import Typist from "react-typist";
+import "react-typist/dist/Typist.css";
+import { motion } from "framer-motion";
 const Home = (props) => {
-    console.log(props);
+  console.log(props);
 
-    return (
+  return (
+    <motion.div
+      className="homeWrapper"
+      initial="initial"
+      animate="in"
+      exit="out"
+      variants={props.animate}
+    >
+      <Typist cursor startDelay={2000}>
+        <span>Hello, my name is Emin Aydın. I'm a frontend devel</span>
+        <Typist.Backspace count={14} delay={500} />
+        <span>cat lover and frontend developer.</span>
+        <br />
+        <br />
 
-
-        <div class="info">
-            <Typist cursor><span>Hello, my name is Emin Aydın.  I'm a full-stack developer who likes cats.</span><br /><br />
-                <span>Check out my latest works at my portfolio page.</span><br /><br />
-                <span>Feel free to contact me through eminaydin93@yahoo.com or from my contact page.</span><br /><br />
-            </Typist >
-
-        </div>
-
-    );
-}
+        <span>You can check out my portfolio page to see my work.</span>
+        <br />
+        <br />
+        <span>Or drop me a message</span>
+        <br />
+        <br />
+      </Typist>
+    </motion.div>
+  );
+};
 
 export default Home;
-
