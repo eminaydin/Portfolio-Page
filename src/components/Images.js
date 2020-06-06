aa9f239c42d64d84e26cf6b4dd427efeb67bd9b4
eminaydin <eminaydin93@yahoo.com>
2020-06-06 12:48:37 +0200

    Import images

---
+++ src/components/Images.js
@@ -0,0 +1,11 @@
+export const images = {
+    0: "./FormValidation.png",
+    1: "./GithubUserFinder.png",
+    2: "./UserDescriber.png",
+    3: "./ColorGenerator.png",
+    4: "./PasswordGenerator.png",
+    5: "./ProductList.png",
+    6: "./Modal.png",
+    7: "./BlogComments.png",
+    8: "./WeatherApp.png"
+}
