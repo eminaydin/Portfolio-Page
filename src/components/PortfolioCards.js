150231753b9f8189ab57d477570c7976d3527390
eminaydin <eminaydin93@yahoo.com>
2020-06-06 12:48:54 +0200

    Portfolio cards

--- src/components/PortfolioCards.js
+++ src/components/PortfolioCards.js
@@ -1,60 +1,78 @@
-import React, { useState } from 'react';
-import {
-
-    Icon,
-    Card,
-    Image,
-} from "semantic-ui-react";
+import React, { useState } from "react";
+import { Container, Icon, Card, Image, Header, Divider } from "semantic-ui-react";
 import data from "../data.json";
-import Portfolio from './Portfolio';
+import Portfolio from "./Portfolio";
+import { images } from "./Images";
+import { useEffect } from "react";
+import Moment from "react-moment";
+
 
 const PortfolioCards = (props) => {
-    const [index, setIndex] = useState(1);
-    const [isClicked, setIsClicked] = useState(false);
-    const clickHandler = (index) => {
-        setIndex(index);
-        setIsClicked(!isClicked)
-    };
-    const callbackFunction = (childData) => {
-        setIsClicked(childData)
-    }
-    return (
-        <Card.Group itemsPerRow="3" centered >
-            {data.map((e) => {
-                return (
-                    <Card raised >
-                        <Image
-                            src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
-                            wrapped
-                            ui={false}
-                            onClick={() => {
-                                const index = data.indexOf(e);
-                                clickHandler(index);
-                            }}
-                        />
-                        <Card.Content>
-                            <Card.Header></Card.Header>
-                            <Card.Meta>
-                                <span className="date">{e.name}</span>
-                            </Card.Meta>
-                            <Card.Description>
-                                Matthew is a musician living in Nashville.
+  const [index, setIndex] = useState(1);
+  const [isClicked, setIsClicked] = useState(false);
+  const [state, setState] = useState(data);
+
+  const clickHandler = (index) => {
+    setIndex(index);
+    setIsClicked(!isClicked);
+  };
+  const callbackFunction = (childData) => {
+    setIsClicked(childData);
+  };
+
+  return (
+    <Container textAlign="center" >
+      <Divider horizontal >
+        <Header as="h4" inverted>
+          <Icon name="tag" />
+            Some of my work
+          </Header>
+      </Divider>
+
+      <Card.Group itemsPerRow="3" centered>
+        {data.map((e) => {
+          const dateToFormat = <Moment fromNow> {e.created_at}</Moment>;
+          const userIndex = data.indexOf(e);
+
+          return (
+            <Card raised >
+              <Image
+                src={require(`${images[userIndex]}`)}
+
+                wrapped
+                size="large"
+                ui={false}
+                onClick={() => {
+                  const index = data.indexOf(e);
+                  clickHandler(index);
+                }}
+              />
+              <Card.Content>
+                <Card.Header></Card.Header>
+                <Card.Meta>
+                  <span className="date">{e.name}</span>
+                </Card.Meta>
+                <Card.Description>
+                  {e.description}
                 </Card.Description>
-                        </Card.Content>
-                        <Card.Content extra>
-                            <a>
-                                <Icon name="user" />
-                  22 Friends
+              </Card.Content>
+              <Card.Content extra>
+                <a>
+                  <Icon name="user" />
+                  {dateToFormat}
                 </a>
-                        </Card.Content>
-                    </Card>
-                );
-            })}
-            {isClicked ? <Portfolio index={index} parentCallback={callbackFunction} /> : null}
+              </Card.Content>
+            </Card>
+          );
+        })}
+        {isClicked ? (
+          <Portfolio index={index} parentCallback={callbackFunction} />
+        ) : null}
+      </Card.Group>
 
-        </Card.Group>
+    </Container>
 
-    );
-}
+  );
+};
 
 export default PortfolioCards;
