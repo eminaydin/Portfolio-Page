4dd45f8d0b9f04e7a513ca805cc9b2ef6a469daf
eminaydin <eminaydin93@yahoo.com>
2020-10-17 21:04:00 +0200

    Clear project

--- src/App.js
+++ src/App.js
@@ -1,46 +1,8 @@
 import React from "react";
-import 'font-awesome/css/font-awesome.min.css';
-import "./App.css"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import 'semantic-ui-css/semantic.min.css'
-import PortfolioCards from "./components/PortfolioCards";
-import Navbar from "./components/Navbar";
-import { AnimatePresence } from "framer-motion";
+import "./App.css";
 
-const App = () => {
-  const pageVariants = {
-    initial: {
-      opacity: 0,
-    },
-    in: {
-      opacity: 1,
-    },
-    out: {
-      opacity: 0,
-    }
-  }
-
-  return (
-    <Router>
-      <Navbar />
-      <AnimatePresence exitBeforeEnter>
-        <Switch>
-          <Route path="/contact" render={() => <Contact animate={pageVariants} />} />
-          <Route path="/portfolio" render={() => <PortfolioCards animate={pageVariants} />} />
-          <Route path="/about" render={() => <About animate={pageVariants} />} />
-          <Route exact path="/" render={() => <Home animate={pageVariants} />} />
-        </Switch>
-      </AnimatePresence>
-    </Router>
-
-  );
+function App() {
+  return <div className="App"></div>;
 }
 
 export default App;
