38864482832ea92f828337591675d383d8ae9371
eminaydin <eminaydin93@yahoo.com>
2020-10-18 22:13:30 +0200

    Provide more information

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Header from "./components/Header";
 import Information from "./components/Information";
 import { ParallaxProvider } from "react-scroll-parallax";
-import AboutMe from "./components/AboutMe";
 function App() {
   return (
     <ParallaxProvider>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import photo from "../assets/image2.jpeg";
 import { Parallax } from "react-scroll-parallax";
 import Typist from "react-typist";
+import UseAnimations from "react-useanimations";
+import github from "react-useanimations/lib/github";
+import linkedin from "react-useanimations/lib/linkedin";
 const Header = () => {
   const [count, setCount] = useState(1);
   useEffect(() => {
@@ -11,10 +14,27 @@ const Header = () => {
     <div className="header">
       <Parallax className="custom-class" y={[-20, 20]} tagOuter="figure">
         <img src={photo} className="profile-photo" />
+        <div className="social-icons">
+          <UseAnimations
+            animation={github}
+            size={56}
+            strokeColor="#58656d"
+            wrapperStyle={{ padding: 100, padding: "unset" }}
+            className="github"
+          />
+          <UseAnimations
+            animation={linkedin}
+            size={56}
+            strokeColor="#58656d"
+            wrapperStyle={{ padding: 100, padding: "unset" }}
+            className="linkedin"
+          />
+        </div>
       </Parallax>
       <div className="main-text-container">
         <h2 className="main-header">HI THERE !</h2>
         <span className="short-description">
+          I'M{" "}
           <Typist
             key={count}
             cursor={{ show: false, blink: false }}
@@ -24,7 +44,7 @@ const Header = () => {
               }, 300);
             }}
           >
-            I'M <span> EMIN AYDIN</span>
+            <span> EMIN AYDIN</span>
             <Typist.Backspace count={10} delay={200} />
             <span>A DEVELOPER</span>
             <Typist.Backspace count={11} delay={200} />
@@ -32,14 +52,19 @@ const Header = () => {
             <Typist.Backspace count={11} delay={200} />
             <span>A CAT LOVER</span>
             <Typist.Backspace count={11} delay={200} />
-            <span>A HISTORIAN</span>
-            <Typist.Backspace count={10} delay={200} />
+            <span>A FOOTBALL FAN</span>
+            <Typist.Backspace count={14} delay={200} />
+            <span>A TECH ENTHUSIAST</span>
+            <Typist.Backspace count={17} delay={200} />
           </Typist>
         </span>
         <p className="long-description">
-          I'm a Freelance UI/UX Designer and Developer based in London, England.
-          I strives to build immersive and beautiful web applications through
-          carefully crafted code and user-centric design.
+          I'm a Berlin based Frontend Developer. I help companies to build
+          unique and fascinating products. I have a strong interest in tech,
+          therefore I am always looking forward to improve myself. I'm mainly
+          working on React, React-Native and Flutter at the moment. If you are
+          already here, I will be more than happy to be connected. Just reach
+          out to me!
         </p>
       </div>
     </div>
