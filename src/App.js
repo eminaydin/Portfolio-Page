7fc07abac591010a7668268ea0918eca828082b2
eminaydin <eminaydin93@yahoo.com>
2020-10-18 17:40:36 +0200

    About me section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
-
+import Header from "./components/Header";
+import Information from "./components/Information";
+import { ParallaxProvider } from "react-scroll-parallax";
+import AboutMe from "./components/AboutMe";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ParallaxProvider>
+      <div className="App">
+        <Header />
+        <Information />
+      </div>
+    </ParallaxProvider>
+  );
 }
 
 export default App;
